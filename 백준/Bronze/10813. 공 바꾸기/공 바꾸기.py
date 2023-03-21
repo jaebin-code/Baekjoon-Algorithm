ad064bc923354512311224d2eaf7a41e8f36fcaa
Jaebin Lee <94092128+jaebin-code@users.noreply.github.com>
2023-03-21 22:23:46 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,7 @@
+n,m=map(int,input().split())
+list1=[i+1 for i in range(n)]
+for i in range(m):
+    a,b=map(int,input().split())
+    list1[a-1],list1[b-1]=list1[b-1],list1[a-1]
+for i in range(len(list1)):
+    print(list1[i],end=" ")
