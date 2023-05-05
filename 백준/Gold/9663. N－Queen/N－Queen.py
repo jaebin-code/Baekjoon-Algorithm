821d10bf26f4b44bf8a7a59403a9c42d73608f19
Jaebin Lee <94092128+jaebin-code@users.noreply.github.com>
2023-05-05 20:08:36 +0900

    [Gold IV] Title: N-Queen, Time: 29448 ms, Memory: 205136 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,19 @@
+def check(x):
+    for i in range(x):
+        if row[x]==row[i] or abs(row[i]-row[x])==x-i:
+            return False
+    return True
+def dfs(x):
+    global result
+    if x==n:
+        result+=1
+    else:
+        for i in range(n):
+            row[x]=i
+            if check(x):
+                dfs(x+1)
+n=int(input())
+row=[0]*n
+result=0
+dfs(0)
+print(result)
