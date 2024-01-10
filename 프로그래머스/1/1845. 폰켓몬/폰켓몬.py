26b5ff4be80b3ab904ffa9bd6a748e036db08346
Jaebin Lee <94092128+jaebin-code@users.noreply.github.com>
2024-01-10 14:44:31 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,2 @@
 def solution(nums):
-    numsLen = len(nums)/2
-    numsList = []
-    answer = 0
-    
-    for i in nums:
-        if i in numsList:
-            continue
-        else:
-            numsList.append(i)
-    if (len(numsList) >= numsLen):
-        return numsLen
-    else:
-        return len(numsList)
+    return min(len(nums)/2, len(set(nums)))
